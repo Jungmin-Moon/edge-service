bb40c9888fb990959e5e4d2ff3ea368aed28f711
Jungmin Moon <jmmoon96@gmail.com>
2025-05-15 22:28:06 -0400

    Learn basics of authentication with an Angular frontend single page

--- edge-service/src/main/java/com/luckycardshop/edge_service/config/SecurityConfig.java
+++ edge-service/src/main/java/com/luckycardshop/edge_service/config/SecurityConfig.java
@@ -1,12 +1,14 @@
 package com.luckycardshop.edge_service.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint;
 import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
 
 @EnableWebFluxSecurity
@@ -16,6 +18,7 @@ public class SecurityConfig {
 	@Bean
 	SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http, ReactiveClientRegistrationRepository clientRegistrationRepository) {
 		return http.authorizeExchange(exchange -> exchange.anyExchange().authenticated())
+				.exceptionHandling(exceptionHandling -> exceptionHandling.authenticationEntryPoint(new HttpStatusServerEntryPoint(HttpStatus.UNAUTHORIZED)))
 				.oauth2Login(Customizer.withDefaults()) //we are using oauth2 login instead
 				.logout(logout -> logout.logoutSuccessHandler(oidcLogoutSuccessHandler(clientRegistrationRepository))) //custom logout handler
 					.build();
